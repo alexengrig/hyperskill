987c60060c6946b07b812699258ffdfa2a192d19
Grig Alex <alexengrigdev@gmail.com>
2019-10-03 21:14:45 +0300

    Resolve Interval problem

--- Problems/Interval/src/Main.java
+++ Problems/Interval/src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+        boolean result = value > -15 && value <= 12
+                || value > 14 && value < 17
+                || value >= 19;
+        System.out.println(result ? "True" : "False");
     }
 }
