624cb52e507a7d25fd581cfadb98fc66be0b6699
Grig Alex <alexengrigdev@gmail.com>
2019-10-12 18:22:27 +0300

    Resolve "The roots of an equation" problem

---
+++ Problems/The roots of an equation/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+        for (int x = 0; x < 1000; x++) {
+            if (a * x * x * x + b * x * x + c * x + d == 0) {
+                System.out.println(x);
+            }
+        }
+    }
+}
