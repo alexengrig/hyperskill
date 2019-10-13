0f5836f9ac3e8ca709b4790164cc903a876f55d1
Grig Alex <alexengrigdev@gmail.com>
2019-10-13 22:14:19 +0300

    Resolve "Encryption-Decryption" Stage 2

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,31 +1,23 @@
 package encryptdecrypt;
 
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Encryptor encryptor = new Encryptor();
-        System.out.println(encryptor.encrypt("we found a treasure!"));
-    }
-
-    static class Encryptor {
-        private static final String ALPHABET = IntStream.rangeClosed('a', 'z')
-                .mapToObj(Character::toString)
-                .collect(Collectors.joining());
-        private static final int ALPHABET_LENGTH = ALPHABET.length();
-
-        public String encrypt(String input) {
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < input.length(); i++) {
-                int index = ALPHABET.indexOf(input.charAt(i));
-                if (index >= 0) {
-                    builder.append(ALPHABET.charAt(ALPHABET_LENGTH - index - 1));
-                } else {
-                    builder.append(input.charAt(i));
-                }
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        int key = scanner.nextInt();
+        String cyphertext = "";
+        for (int i = 0, l = text.length(); i < l; i++) {
+            char ch = text.charAt(i);
+            int index = alphabet.indexOf(ch);
+            if (index > -1) {
+                cyphertext += alphabet.charAt((index + key) % alphabet.length());
+            } else {
+                cyphertext += ch;
             }
-            return builder.toString();
         }
+        System.out.println(cyphertext);
     }
 }
