f0d25ae7b51640a4b78292597b7f1c098859a963
Grig Alex <alexengrigdev@gmail.com>
2019-10-19 21:24:17 +0300

    Resolve "Preparing full name" problem

---
+++ Problems/Preparing full name/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static String prepareFullName(String firstName, String lastName) {
+        if (firstName != null && lastName != null) {
+            return firstName + " " + lastName;
+        } else if (firstName != null) {
+            return firstName;
+        } else {
+            return lastName;
+        }
+    }
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+
+        String firstName = scanner.nextLine();
+        firstName = "null".equals(firstName) ? null : firstName;
+
+        String lastName = scanner.nextLine();
+        lastName = "null".equals(lastName) ? null : lastName;
+
+        System.out.println(prepareFullName(firstName, lastName));
+    }
+}
